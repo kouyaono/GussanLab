b7c620ec688219fab5f932e4aed76a27caec9c91
kouyaono <w11dax.gj22@gmail.com>
2013-12-08 18:20:05 +0900

    practice2 & practice3 in GussanLab

---
+++ practice2.php
@@ -0,0 +1,12 @@
+<?
+	$a = 8;
+	$b = 5;
+	$sum = $a + $b;
+	$sa = $a - $b;
+	$syo = $a / $b;
+	$seki = $a * $b;
+	echo "和:".$sum."\n";
+	echo "差:".$sa."\n";
+	echo "積:".$seki."\n";
+	echo "商:".$syo."\n";
+	
